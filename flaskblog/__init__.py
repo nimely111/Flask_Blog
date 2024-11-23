49e07ad01f884699d773ab3288c161c7042ff1a7
nimely111 <samuelehnimely@gmail.com>
2024-11-23 14:27:58 +0000

    update file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,3 +9,5 @@ app.config['SECRET_KEY'] = '8e4268d5e6fa9ba74a5ba9463d833c35'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # sqlalchemy db instance
 db = SQLAlchemy(app)
+
+from flaskblog import routes
