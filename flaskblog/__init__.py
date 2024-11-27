df58781f318f481ba8e2177d70b7cfcb98b7fb1f
nimely111 <samuelehnimely@gmail.com>
2024-11-27 03:48:12 +0000

    import and create an instance of the flask_bcrypt for password harsing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '8e4268d5e6fa9ba74a5ba9463d833c35'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # sqlalchemy db instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
