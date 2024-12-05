2dc14799c0ce2dbd8cf94b6638539573c0c3765d
Nimely111 <samuelehnimely@gmail.com>
2024-12-05 01:54:33 +0000

    setup app to use flask-migrate

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,16 +3,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
 #application configurations
 app.config['SECRET_KEY'] = '8e4268d5e6fa9ba74a5ba9463d833c35'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # flaskblog instances
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+migrate = Migrate(app, db)
 
 from flaskblog import routes
