f87eb53fdf1fe3a46cd6a4f319b9be5dbc7807ca
Nimely111 <samuelehnimely@gmail.com>
2024-12-05 03:06:07 +0000

    update validation message

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -45,14 +45,14 @@ class RegistrationForm(FlaskForm):
         # checking if the user credentials is already in our db then we get the first user else we get none and the new user signin successfully
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('This username is taken. Please choose a different one.')
     
     # creating a custom valiations
     def validate_email(self, email):
         # checking if the user credentials is already in our db then we get the first user else we get none and the new user signin successfully
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('This email is taken. Please choose a different one.')
     
 # Login form
 class LoginForm(FlaskForm):
