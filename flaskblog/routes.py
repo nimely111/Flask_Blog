7bd3f174e6f97d71b161c71d45141394fca9f99a
Nimely111 <samuelehnimely@gmail.com>
2024-12-08 05:17:27 +0000

    import and add the login required decorator to the account route to protect it from unauthenticated user

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -7,7 +7,7 @@ from flask import (
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [
@@ -76,6 +76,7 @@ def logout():
 
 
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title="Account")
 
