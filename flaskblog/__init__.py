9d88f6d4b85c9111b2d7a79676f91c55b3239dc8
Nimely111 <samuelehnimely@gmail.com>
2024-12-08 05:30:57 +0000

    fixed bug (account is not callable )warning and welcome the current login user in their dashboard

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,7 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view('login') #telling the extension where the login_view is located
+login_manager.login_view ='login' #telling the extension where the login_view is located
 migrate = Migrate(app, db)
 
 from flaskblog import routes
