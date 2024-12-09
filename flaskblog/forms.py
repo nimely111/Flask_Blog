d2e85fed39468d030aefc784c0a3b82aff10e2b2
Nimely111 <samuelehnimely@gmail.com>
2024-12-09 22:43:12 +0000

    add update form for users

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 # import form fields
 from wtforms import (
     StringField,
@@ -73,3 +74,31 @@ class LoginForm(FlaskForm):
 
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+                    'Username', 
+                    validators=[DataRequired(), 
+                    Length(min=2, max=20)]
+                    )
+    email = StringField(
+                    'Email',
+                    validators=[DataRequired(), 
+                                Email()]
+                    )
+    submit = SubmitField('Update')
+    
+    # creating a custom valiation
+    def validate_username(self, username):
+        # checking if the user credentials is already in our db then we get the first user else we get none and the new user signin successfully
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken. Please choose a different one.')
+    
+    # creating a custom valiations
+    def validate_email(self, email):
+        if email.data != current_user.email:
+        # checking if the user credentials is already in our db then we get the first user else we get none and the new user signin successfully
+            user = User.query.filter_by(email=email.data).first()
+            if user: 
+                raise ValidationError('This email is taken. Please choose a different one.')
